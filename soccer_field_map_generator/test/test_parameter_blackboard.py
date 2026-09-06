47fdc81aae422989097ef11dc3976983f9200105
Jan Gutsche <github@jaagut.de>
2026-09-06 12:41:23 +0200

    test: follow upstream test style

--- soccer_field_map_generator/test/test_parameter_blackboard.py
+++ soccer_field_map_generator/test/test_parameter_blackboard.py
@@ -5,15 +5,15 @@ from soccer_field_map_generator.generator import generate_parameter_blackboard
 
 def test_parameter_blackboard():
     result = generate_parameter_blackboard(
-        dict(
-            field_length=1400,
-            field_width=900,
-            border_strip_width=100,
-            penalty_area_length=300,
-            center_circle_diameter=300,
-            goal_width=260,
-            goal_depth=75,
-        )
+        {
+            'field_length': 1400,
+            'field_width': 900,
+            'border_strip_width': 100,
+            'penalty_area_length': 300,
+            'center_circle_diameter': 300,
+            'goal_width': 260,
+            'goal_depth': 75,
+        }
     )
     field = result['parameter_blackboard']['ros__parameters']['field']
     assert field == {
